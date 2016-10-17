e452fcafa76650416f92394495a2baeae77456ab
Jake Whatley <jacob.whatley91@gmail.com>
2016-10-17 19:47:01 +0200

    gps: use all prebuilt from stock 1450
    Change-Id: I0989f3488985cebd315ff3cd1f1552c17ba39cb9

--- device.mk
+++ device.mk
@@ -347,6 +347,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     gps.msm8992
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=0 \
     ro.gps.agps_provider=1
