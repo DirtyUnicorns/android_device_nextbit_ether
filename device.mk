1ba8460cb2e9514fefee64010e1909cd8daf6dfb
Josh Chasky <jchasky@gmail.com>
2016-11-15 11:45:59 -0500

    Ether: Update du.dependencies
    Change-Id: I88d284fb37f9b554c202fdf90d2797d57de33064

--- device.mk
+++ device.mk
@@ -344,9 +344,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
 
-PRODUCT_PACKAGES += \
-    gps.msm8992
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
